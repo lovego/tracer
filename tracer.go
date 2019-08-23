24e629582fcf88db9da6fedffa1ff08c0e9e82a7
bughou <bughou@gmail.com>
2019-08-24 01:56:45 +0800

    refactor

--- tracer.go
+++ tracer.go
@@ -2,56 +2,68 @@ package tracer
 
 import (
 	"context"
+	"fmt"
+	"time"
 )
 
-var spanKey spanKeyStruct
-
-type spanKeyStruct struct {
+type tracer struct {
+	Name     string                 `json:"name,omitempty"`
+	At       time.Time              `json:"at"`
+	Duration float64                `json:"duration"` // milliseconds
+	Children []*tracer              `json:"children,omitempty"`
+	Tags     map[string]interface{} `json:"tags,omitempty"`
+	Logs     []string               `json:"logs,omitempty"`
 }
 
-// Context return a tracer context with the span
-func Context(ctx context.Context, s *Span) context.Context {
-	if s == nil {
-		return ctx
+// Start start a new tracer on the given context
+func Start(ctx context.Context, name string) context.Context {
+	tracer := tracer{Name: name, At: time.Now()}
+
+	if parent := get(ctx); parent != nil {
+		parent.Children = append(parent.Children, tracer)
 	}
-	return context.WithValue(ctx, spanKey, s)
-}
-
-// StartContext start a new tracer context on the given tracer context
-func StartContext(ctx context.Context, name string) context.Context {
-	return context.WithValue(context.Background(), spanKey, StartSpan(ctx, name))
-}
-
-// GetSpan get span from a tracer context
-func GetSpan(ctx context.Context) *Span {
-	if ctx != nil {
-		if v := ctx.Value(spanKey); v != nil {
-			if s, ok := v.(*Span); ok {
-				return s
-			}
-		}
+	if ctx == nil {
+		ctx = context.Background()
 	}
-	return nil
+	return context.WithValue(ctx, key, tracer)
+}
+
+// Finish finish the tracer on the given context
+func Finish(ctx context.Context) {
+	if tracer := get(ctx); tracer != nil {
+		tracer.Duration = float64(time.Since(tracer.At)) / float64(time.Millisecond)
+	}
 }
 
 // Tag add a tag to a tracer context
 func Tag(ctx context.Context, k string, v interface{}) {
-	GetSpan(ctx).Tag(k, v)
+	if tracer := get(ctx); tracer != nil {
+		if tracer.Tags == nil {
+			tracer.Tags = make(map[string]interface{})
+		}
+		tracer.Tags[k] = v
+	}
 }
 
 // DebugTag add a tag to a tracer context if debug is enabled
 func DebugTag(ctx context.Context, k string, v interface{}) {
-	GetSpan(ctx).DebugTag(k, v)
+	if ctx != nil && ctx.Value("debug") != nil {
+		Tag(ctx, k, v)
+	}
 }
 
-// Log add a log to a tracer context
+// Log add a log to a tracer context using Sprint
 func Log(ctx context.Context, args ...interface{}) {
-	GetSpan(ctx).Log(args...)
+	if tracer := get(ctx); tracer != nil {
+		s.Logs = append(s.Logs, fmt.Sprint(args...))
+	}
 }
 
-// Logf add a log to a tracer context
+// Logf add a log to a tracer context using Sprintf
 func Logf(ctx context.Context, format string, args ...interface{}) {
-	GetSpan(ctx).Logf(format, args...)
+	if tracer := get(ctx); tracer != nil {
+		s.Logs = append(s.Logs, fmt.Sprintf(format, args...))
+	}
 }
 
 // DebugLog add a log to a tracer context if debug is enabled
@@ -62,4 +74,19 @@ func DebugLog(ctx context.Context, args ...interface{}) {
 // DebugLogf add a log to a tracer context if debug is enabled
 func DebugLogf(ctx context.Context, format string, args ...interface{}) {
 	GetSpan(ctx).DebugLogf(format, args...)
+}
+
+var key keyType
+
+type keyType struct{}
+
+func get(ctx context.Context) *tracer {
+	if ctx != nil {
+		if v := ctx.Value(key); v != nil {
+			if t, ok := v.(*tracer); ok {
+				return t
+			}
+		}
+	}
+	return nil
 }
